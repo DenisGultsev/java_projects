429e1009ca9c01562a6b65e0b57c6b44058573e2
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-10-26 22:01:41 +1000

    Hello World

---
+++ lesson1.java
@@ -0,0 +1,3 @@
+﻿public class lesson1 {
+
+}
---
+++ program.java
@@ -0,0 +1,67 @@
+﻿
+public class program {
+    public static void main(String[] args) {
+        System.out.println("Привет, Денис!"); // приветствие
+    }
+}
+
+/*
+ * public class program {
+ * public static void main(String[] args) {
+ * String s = null;
+ * System.out.println(s);
+ * }
+ * 
+ * }
+ */
+
+/*
+ * public class program {
+ * public static void main(String[] args) {
+ * float a = 123.45f;
+ * double f = 123.45;
+ * System.out.println(a);
+ * System.out.println(f);
+ * }
+ * }
+ */
+/*
+ * public class program {
+ * public static void main(String[] args) {
+ * boolean flag1 = 123 <= 234;
+ * System.out.println(flag1);
+ * boolean flag2 = 123 >= 234 || flag1;
+ * System.out.println(flag2);
+ * boolean flag3 = flag1 ^ flag2;
+ * System.out.println(flag3);
+ * }
+ * }
+ */
+
+/**
+ * program
+ */
+
+/*
+ * public class program {
+ * 
+ * public static void main(String[] args) {
+ * String msg = "Привет Денис!";
+ * System.out.println(msg);
+ * }
+ * }
+ */
+
+/**
+ * program
+ */
+
+/*
+ * public class program {
+ * 
+ * public static void main(String[] args) {
+ * var i = 123;
+ * System.out.println(i);
+ * }
+ * }
+ */
